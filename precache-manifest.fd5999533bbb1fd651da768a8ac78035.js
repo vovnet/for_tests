bdc48a7fae70abf9ae62d51ddfb9d54776878247
vovnet <vovnet@yandex.ru>
2019-08-15 10:09:26 +0300

    Init

---
+++ precache-manifest.fd5999533bbb1fd651da768a8ac78035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061079105add09c1eb5669d165911cf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3b9689885bda11a376b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "43fa8760a51ccfa07b48",
+    "url": "/static/js/2.2f55d8df.chunk.js"
+  },
+  {
+    "revision": "a3b9689885bda11a376b",
+    "url": "/static/js/main.dee1e1b1.chunk.js"
+  },
+  {
+    "revision": "a4db827d3d945cb77b03",
+    "url": "/static/js/runtime~main.265d1835.js"
+  }
+]);
